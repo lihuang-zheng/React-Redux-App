215cf5405704b4f578ed220dcd8825cab8e568e7
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-19 16:29:20 -0600

    added react player to play the video.

---
+++ client/src/components/DogImg.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+// import connect for connect it to the store
+import { connect } from "react-redux"
+
+// import action
+import { getDogData } from "../actions"
+
+
+
+
+const DogImg = props => {
+
+}
