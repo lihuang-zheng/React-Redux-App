ef3d4e902f5f7800a135a46d0a3cd0dba8efcb6a
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-19 16:15:55 -0600

    structured the basic of redux in app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 
+// redux imports here
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+
+// import reducer here
+
+
+// created store for reducer
+const store = createStore(reducer, applyMiddleware(thunk))
+
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
 
-    </div>
+      </div>
+    </Provider>
   );
 }
 
